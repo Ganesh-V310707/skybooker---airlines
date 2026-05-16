414b574791d01b623f0f6675c7fc961ca9a95b3b
GANESH.V <ganeshvs310707@gmail.com>
2026-05-17 00:52:10 +0530

    Fixed production CORS

--- server/server.js
+++ server/server.js
@@ -27,7 +27,13 @@ const authRoutes =
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin:
+      "https://skybooker-airlines-1.onrender.com",
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 
