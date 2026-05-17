8bb710b840246bf5917d2223dc02b0ca5f638610
GANESH.V <ganeshvs310707@gmail.com>
2026-05-17 08:20:30 +0530

    fixed deployment

--- server/server.js
+++ server/server.js
@@ -1,36 +1,25 @@
 const express = require("express");
-
 const cors = require("cors");
-
 require("dotenv").config();
 
-const sequelize =
-  require("./config/db");
+const sequelize = require("./config/db");
 
-const Flight =
-  require("./models/Flight");
+const Flight = require("./models/Flight");
+const User = require("./models/User");
+const Booking = require("./models/Booking");
 
-const User =
-  require("./models/User");
-
-const Booking =
-  require("./models/Booking");
-
-const flightRoutes =
-  require("./routes/flightRoutes");
-
-const bookingRoutes =
-  require("./routes/bookingRoutes");
-
-const authRoutes =
-  require("./routes/authRoutes");
+const flightRoutes = require("./routes/flightRoutes");
+const bookingRoutes = require("./routes/bookingRoutes");
+const authRoutes = require("./routes/authRoutes");
 
 const app = express();
 
 app.use(
   cors({
-    origin:
+    origin: [
+      "http://localhost:5173",
       "https://skybooker-airlines-1.onrender.com",
+    ],
     credentials: true,
   })
 );
@@ -53,47 +42,27 @@ Booking.belongsTo(Flight, {
   foreignKey: "flightId",
 });
 
-app.use(
-  "/api/flights",
-  flightRoutes
-);
+app.use("/api/flights", flightRoutes);
 
-app.use(
-  "/api/bookings",
-  bookingRoutes
-);
+app.use("/api/bookings", bookingRoutes);
 
-app.use(
-  "/api/auth",
-  authRoutes
-);
+app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => {
-
-  res.send(
-    "SkyBooker Backend Running"
-  );
+  res.send("SkyBooker Backend Running");
 });
 
-sequelize.sync()
+sequelize
+  .sync()
   .then(() => {
-
-    console.log(
-      "PostgreSQL Connected"
-    );
-
+    console.log("PostgreSQL Connected");
   })
   .catch((err) => {
-
     console.log(err);
   });
 
-const PORT =
-  process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-
-  console.log(
-    `Server running on port ${PORT}`
-  );
+  console.log(`Server running on port ${PORT}`);
 });
